fb501bb1d6b84c8739d7e5002c66cd02d902c9c7
Antionix <persuch@inbox.ru>
2020-04-30 02:07:13 +0300

    Recover

--- src/main/java/ClientSwingUI/ChatFrame.java
+++ src/main/java/ClientSwingUI/ChatFrame.java
@@ -8,5 +8,38 @@ import java.awt.event.ActionListener;
 import static java.lang.String.format;
 
 public class ChatFrame extends JFrame {
+    private JTextArea textArea;
+    private JTextField inputTextField;
+    private JButton sendButton;
 
+    public ChatFrame() {
+        buildGUI();
+    }
+
+    private void buildGUI() {
+        textArea = new JTextArea(20, 50);
+        textArea.setEditable(false);
+        textArea.setLineWrap(true);
+        add(new JScrollPane(textArea), BorderLayout.CENTER);
+
+        Box box = Box.createHorizontalBox();
+        add(box, BorderLayout.SOUTH);
+        inputTextField = new JTextField();
+        sendButton = new JButton("Send");
+        box.add(inputTextField);
+        box.add(sendButton);
+
+        ActionListener sendListener = (ActionEvent e) -> {
+            String inputText = inputTextField.getText();
+            if (inputText != null && inputText.trim().length() > 0) {
+                textArea.append(format("%s\n", inputText));
+
+                inputTextField.selectAll();
+                inputTextField.requestFocus();
+                inputTextField.setText("");
+            }
+        };
+        inputTextField.addActionListener(sendListener);
+        sendButton.addActionListener(sendListener);
+    }
 }
--- src/main/java/ClientSwingUI/Main.java
+++ src/main/java/ClientSwingUI/Main.java
@@ -4,6 +4,12 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-
+        JFrame frame = new ChatFrame();
+        frame.setTitle("MyChatLan");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setVisible(true);
     }
 }
